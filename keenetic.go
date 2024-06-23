7fe4a51fc410dedb042284029b200631d28f73bc
mimimix <maksbubnov.07@gmail.com>
2024-06-23 22:52:47 +0300

    fix: add error codes

--- keenetic.go
+++ keenetic.go
@@ -1,6 +1,8 @@
 package keenetic
 
 import (
+	"errors"
+	"fmt"
 	"github.com/imroc/req/v3"
 	"net/http"
 )
@@ -12,6 +14,8 @@ type Keenetic struct {
 	Request  *req.Client
 }
 
+var ErrInvalidCode = errors.New("invalid code")
+
 func NewKeenetic(username, password, baseUrl string) *Keenetic {
 	zyxel := &Keenetic{
 		username: username,
@@ -21,17 +25,27 @@ func NewKeenetic(username, password, baseUrl string) *Keenetic {
 	zyxel.Request = req.C().SetBaseURL(baseUrl).
 		//SetCommonHeader("Content-Type", "application/json;charset=UTF-8").
 		AddCommonRetryCondition(func(resp *req.Response, err error) bool {
+			if !errors.Is(err, ErrInvalidCredentials) {
+				return false
+			}
 			return resp.StatusCode == http.StatusUnauthorized
 		}).
 		SetCommonRetryCount(2).
-		SetCommonRetryHook(zyxel.reauthManager)
+		SetCommonRetryHook(func(resp *req.Response, _ error) {
+			isSuccess, cookies := zyxel.Login()
+			if isSuccess == nil {
+				resp.Request.SetCookies(cookies...)
+			}
+		}).
+		OnAfterResponse(func(client *req.Client, resp *req.Response) error {
+			if resp.StatusCode != http.StatusOK {
+				if resp.StatusCode == http.StatusUnauthorized {
+					return ErrInvalidCredentials
+				}
+				return fmt.Errorf("%w: %d", ErrInvalidCode, resp.StatusCode)
+			}
+			return nil
+		})
 
 	return zyxel
-}
-
-func (zyxel *Keenetic) reauthManager(resp *req.Response, _ error) {
-	isSuccess, cookies := zyxel.Login()
-	if isSuccess == nil {
-		resp.Request.SetCookies(cookies...)
-	}
 }
